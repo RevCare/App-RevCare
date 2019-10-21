2a96303febe53ea23c47862193060baa00a69c2a
Jaime Victor <victoriotak@gmail.com>
2019-10-20 21:25:23 -0300

    Revert "Revert "Merge pull request #1 from RevCare/Ana""
    This reverts commit 1330de5b1f8a2827077676e39f37b19b2f6ae73a.

--- app/src/main/java/br/ufrpe/revcare/infra/gui/CadastroUsuario1Activity.java
+++ app/src/main/java/br/ufrpe/revcare/infra/gui/CadastroUsuario1Activity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
 import android.widget.Button;
 import android.widget.EditText;
 
 import br.ufrpe.revcare.R;
+import br.ufrpe.revcare.usuario.dominio.Usuario;
+import br.ufrpe.revcare.usuario.persistencia.UsuarioDAO;
 
 public class CadastroUsuario1Activity extends AppCompatActivity {
 
@@ -20,12 +26,40 @@ public class CadastroUsuario1Activity extends AppCompatActivity {
     private EditText nTelefone;
     private EditText nSenha;
     private EditText nConfirmarSenha;
+    private UsuarioDAO dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_cadastro_usuario1);
+
+        nNome = findViewById(R.id.nomeTextField);
+        nTelefone = findViewById(R.id.telefoneTextField);
+        nCpf = findViewById(R.id.cpfTextField);
+        nEndereco = findViewById(R.id.enderecoTextField);
+        nSenha = findViewById(R.id.senhaTextField);
+        nEmail = findViewById(R.id.emailTextField);
+        nDataNascimento = findViewById(R.id.dataNascimentoTextField);
+
+        dao = new UsuarioDAO(this);
+
+    }
+    public void cadastrar(View view){
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(nNome.getText().toString());
+        usuario.setCpf(nCpf.getText().toString());
+        usuario.setEndereco(nEndereco.getText().toString());
+        usuario.setTelefone(nTelefone.getText().toString());
+        usuario.setEmail(nEmail.getText().toString());
+        usuario.setDataNascimento(nDataNascimento.getText().toString());
+        usuario.setSenha(nSenha.getText().toString());
+
+        long id = dao.cadastrarUsuario(usuario);
+
+        Toast.makeText(this, "Usuario salvo com sucesso " + id, Toast.LENGTH_SHORT).show();
+
         Button botao_continuar_cadastro = findViewById(R.id.botaoFinalizarCadastro);
         botao_continuar_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
