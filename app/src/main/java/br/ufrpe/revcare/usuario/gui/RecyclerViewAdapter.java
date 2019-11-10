7a2de17283ed16c2a9e35733c93cfd3ed2db170b
nekojess1 <jessi_alves1@outlook.com>
2019-11-10 19:35:22 -0300

    O button do recyclerview tá pegando, scrr

--- app/src/main/java/br/ufrpe/revcare/usuario/gui/RecyclerViewAdapter.java
+++ app/src/main/java/br/ufrpe/revcare/usuario/gui/RecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -12,9 +13,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 import br.ufrpe.revcare.R;
+import br.ufrpe.revcare.infra.gui.MainActivity;
+import br.ufrpe.revcare.profissional.gui.CadastroProfissional;
 import br.ufrpe.revcare.profissional.gui.PerfilProfissional;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>  {
@@ -35,9 +40,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return holder;
     }
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.nome.setText(mNome.get(position));
         holder.descricao.setText(mDescricao.get(position));
+        holder.btnVerMais.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, PerfilProfissional.class);
+                mContext.startActivity(intent);
+            }
+
+        });
+
 
     }
 
@@ -50,13 +65,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView nome;
         TextView descricao;
         RelativeLayout parentLayout;
-        Button btnInfo;
+        Button btnVerMais;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             nome = itemView.findViewById(R.id.nome);
             descricao = itemView.findViewById(R.id.localizacao);
             parentLayout = itemView.findViewById(R.id.parent_layout);
+            btnVerMais = itemView.findViewById(R.id.btnVerMais);
         }
     }
 }
