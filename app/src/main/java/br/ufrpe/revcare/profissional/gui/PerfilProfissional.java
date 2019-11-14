e1bcae571ece805e2f7702e9895b744b1dfb58d2
guilherme-marinho <guilherme.m97@icloud.com>
2019-11-14 09:40:50 -0300

    botão de ver mais funcionando dnv

--- app/src/main/java/br/ufrpe/revcare/profissional/gui/PerfilProfissional.java
+++ app/src/main/java/br/ufrpe/revcare/profissional/gui/PerfilProfissional.java
@@ -6,18 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import br.ufrpe.revcare.R;
 import br.ufrpe.revcare.avaliacao.gui.AvaliacaoProfissional;
-import br.ufrpe.revcare.avaliacao.gui.GuardaProfissional;
 
 public class PerfilProfissional extends AppCompatActivity {
-    public TextView nomeTela = findViewById(R.id.nomeProfissionalPerfil);
-    public TextView enderecoTela = findViewById(R.id.enderecoProfissional);
-    public TextView telefoneTela = findViewById(R.id.telefoneProfissionalHome);
-    public TextView descricaoTela = findViewById(R.id.descricaoprofissionalPerfil);
-    public TextView emailTela = findViewById(R.id.emailProfissionalPerfil);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +26,6 @@ public class PerfilProfissional extends AppCompatActivity {
         btnAvaliarProfissional.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                GuardaProfissional guardaProfissional = new GuardaProfissional();
-                String email = emailTela.getText().toString();
-                guardaProfissional.setProfissionalGuardado(email,getApplicationContext());
                 startActivity(new Intent(PerfilProfissional.this, AvaliacaoProfissional.class));
             }
         });
@@ -48,6 +43,11 @@ public class PerfilProfissional extends AppCompatActivity {
         }
     }
     private void mudar_dados(String nome, String endereco, String telefone, String email, String descricao){
+        TextView nomeTela = findViewById(R.id.nomeProfissionalPerfil);
+        TextView enderecoTela = findViewById(R.id.enderecoProfissional);
+        TextView telefoneTela = findViewById(R.id.telefoneProfissionalHome);
+        TextView descricaoTela = findViewById(R.id.descricaoprofissionalPerfil);
+        TextView emailTela = findViewById(R.id.emailProfissionalPerfil);
         enderecoTela.setText(endereco);
         nomeTela.setText(nome);
         telefoneTela.setText(telefone);
