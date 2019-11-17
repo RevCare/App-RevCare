9b448e8f1c6250f3f4c2d033ae707dbf28d4b559
Ana <anappn19@gmail.com>
2019-11-17 14:04:14 -0300

    foto esta ficando no holder

--- app/src/main/java/br/ufrpe/revcare/profissional/gui/HomeProfissional.java
+++ app/src/main/java/br/ufrpe/revcare/profissional/gui/HomeProfissional.java
@@ -129,9 +129,12 @@ public class HomeProfissional extends AppCompatActivity {
         telefone.setText(profissional.getTelefone());
         email.setText(profissional.getEmail());
         descricao.setHint(profissional.getDescricao());
-        //byte[] imagemEmBits = profissional.getFoto();
-        //Bitmap bitmap = BitmapFactory.decodeByteArray(imagemEmBits, 0, imagemEmBits.length);
-        //mImagemCliente.setImageBitmap(bitmap);
+        byte[] imagemEmBits = profissional.getFoto();
+        if (profissional.getFoto() != null){
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imagemEmBits, 0, imagemEmBits.length);
+            mImagemCliente.setImageBitmap(bitmap);
+        }
+
 
     }
     public void atualizarPerfil() {
