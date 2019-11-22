0e3ba71fca43b30c4bc17135839c503a0ca4d75b
jaimevictor <victoriotak@gmail.com>
2019-11-22 08:00:00 -0300

    Revert "Ok, último commit. To com sono e nem lemro oq fiz"
    This reverts commit 2912efb2baf4a7ac9b589fa65ba23960555edd8f.

---
+++ app/src/main/java/br/ufrpe/revcare/usuario/gui/galeria_profissional_recycler.java
@@ -0,0 +1,16 @@
+package br.ufrpe.revcare.usuario.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufrpe.revcare.R;
+
+public class galeria_profissional_recycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galeria_profissional_recycler);
+    }
+}
